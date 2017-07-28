6a02d9a91ca7d3dfc37e2a8de369dab8f7d0f8bb
Johnny Mkhael <johnny.mkhael@gmail.com>
2017-07-28 17:38:32 +0200

    Easing the deployment

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # blog-pinger
 FaaS stack to ping my blog and report on a slack channel if ping fails
 
-1. Deploy faas
-2. Deploy docker-compose (cron)
-3. Adapt the token in samples.yml
-
-4. build and deploy
+1. Adapt the token in samples.yml
+2. build and deploy
 ```
 ./build.sh
 ./deploy.sh
 ```
-5. `./test.sh`
+
+3. `./test.sh`
--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,17 @@
+docker stack deploy -c docker-compose.yml func
+
+sleep 10
+
+docker service create --name cronjob \
+    --network func_functions --replicas 0 \
+    --restart-condition=none \
+    -l "com.df.notify=true" \
+    -l "com.df.cron=true" \
+    -l "com.df.cron.name=cronjob" \
+    -l "com.df.cron.image=alpine" \
+    -l "com.df.cron.command=echo Hello World" \
+    -l "com.df.cron.schedule=@every 10s" \
+    alpine \
+    wget http://gateway:8080/function/url_ping --post-data="http://jmkhael.io/"
+
 faas-cli -action deploy -f ./samples.yml
